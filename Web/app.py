e1663fad8dac4b665ad8bf7430e9882e9e6e553f
Ujwal Karippali Chandran <43445980+ujwalkpl@users.noreply.github.com>
2019-09-15 06:47:16 +0530

    Add files via upload

---
+++ Web/app.py
@@ -0,0 +1,54 @@
+import numpy as np
+from flask import Flask,request,jsonify,render_template
+import pickle
+import datetime
+from datetime import date
+import calendar
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    datea = request.form.values()
+
+    output1 = next(datea)
+    a = list(map(int,output1.split('-')))
+
+    def findDay(x,y,z):
+
+        born = datetime.date(x, y, z)
+        return born.strftime("%A")
+
+        # Driver program
+
+
+    day=findDay(a[0],a[1],a[2])
+    output=day
+    dict={'Sunday': 6,'Monday':0,'Tuesday':1,'Wednesday':2,'Thursday':3,'Friday':4,'Saturday':5}
+    fday=dict[day]
+
+
+
+
+    output2=int(next(datea))
+
+    # final_features = [np.array(int_features)]
+    prediction = model.predict([[a[2],output2,fday,a[1],a[0]]])
+
+    output = round(prediction[0], 2)
+    profit=round(output-output2)/(output2/100)
+
+    # output = datea[0]
+    return render_template('index.html', prediction_text='Closing price {}'.format(output),predictionprofit='Profit percentage :{}'.format(profit) if profit>0 else 'Percentage of Loss {}'.format((-1*profit)))
+    # return render_template('index.html',prediction_text="Awesome")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Web/train.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+
+df=pd.read_csv('samsu.csv')
+df.Open=df.Open.fillna(df.Open.mean())
+df.Close=df.Close.fillna(df.Close.mean())
+
+Dat=list(pd.DatetimeIndex(df['Date']).day)
+Dat=np.reshape(Dat,(len(Dat),1))
+OpenPrice=list(df['Open'])
+df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+df['Day']=df['Date'].dt.dayofweek
+df['Month']=pd.DatetimeIndex(df['Date']).month
+df['Year']=pd.DatetimeIndex(df['Date']).year
+df['Date']=Dat
+df['Month']=pd.DatetimeIndex(df['Date']).month
+
+df.drop(['High','Low','Adj Close','Volume'],axis=1,inplace=True);
+
+Y=df['Close'].values
+X=df.drop('Close',axis=1).values
+
+X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.33, random_state=42)
+
+from sklearn.linear_model import LinearRegression
+
+clfreg = LinearRegression(n_jobs=-1)
+clfreg.fit(X_train, y_train)
+pickle.dump(clfreg,open('model.pkl','wb'))
+y_pred=clfreg.predict(X_test)
+confidencereg = clfreg.score(X_test, y_test)
+print(confidencereg)
