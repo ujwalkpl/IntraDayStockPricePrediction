d86127cb6d78be26e0b9ce725fad156845cc4919
Ujwal Karippali Chandran <43445980+ujwalkpl@users.noreply.github.com>
2019-09-15 07:13:33 +0530

    Add files via upload

--- Web/train.py
+++ Web/train.py
@@ -25,11 +25,18 @@ X=df.drop('Close',axis=1).values
 
 X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.33, random_state=42)
 
-from sklearn.linear_model import LinearRegression
+# from sklearn.linear_model import LinearRegression
+#
+# clfreg = LinearRegression(n_jobs=-1)
+# clfreg.fit(X_train, y_train)
+# pickle.dump(clfreg,open('model.pkl','wb'))
+# y_pred=clfreg.predict(X_test)
+# confidencereg = clfreg.score(X_test, y_test)
+# print(confidencereg)
 
-clfreg = LinearRegression(n_jobs=-1)
-clfreg.fit(X_train, y_train)
-pickle.dump(clfreg,open('model.pkl','wb'))
-y_pred=clfreg.predict(X_test)
-confidencereg = clfreg.score(X_test, y_test)
-print(confidencereg)
+from sklearn.neural_network import MLPRegressor
+clf = MLPRegressor(alpha=1e-5, hidden_layer_sizes=(10), random_state=1)
+clf.fit(X_train,y_train)
+pickle.dump(clf,open('model.pkl','wb'))
+y_pred=clf.predict(X_test)
+clf.score(X_test,y_test)
